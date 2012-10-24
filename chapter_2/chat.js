a8d1f4dcf003fffec02bbed635022409544955c6
David Lusby <lusbyd@gmail.com>
2012-10-25 00:01:45 +0100

    Write out the data received to the console
    currently garbled nonsense

--- chapter_2/chat.js
+++ chapter_2/chat.js
@@ -2,9 +2,12 @@ var net = require('net')
 
 var chatServer = net.createServer()
 
-chatServer.on('connection', function(client) {
-	client.write('Hi!\n'); client.write('Bye!\n');
-	client.end() 
+chatServer.on('connection', function(client) { 
+	client.write('Hi!\n'); 
+	
+	client.on('data', function(data) {
+		console.log(data) 
+	})
 })
 
 chatServer.listen(9000)
